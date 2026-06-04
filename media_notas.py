d12302659de143911160b64f45efb077a531b876
Ana <peixotoanaluiza65@gmail.com>
2026-06-04 14:46:47 -0300

    Add files via upload

---
+++ media_notas.py
@@ -0,0 +1,11 @@
+nota1 = float(input('Qual é a primeira nota: '))
+nota2 = float(input('Qual é a segunda nota: '))
+nota3 = float(input('Qual é a terceira nota: '))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7:
+    print('Parabéns voçe foi aprovado!!')
+else:
+    print('Voçe foi reprovado!')
+
