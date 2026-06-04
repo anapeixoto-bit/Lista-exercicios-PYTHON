4f728b06b1f18a1cde6e099261de22cd116a5d0f
Ana <peixotoanaluiza65@gmail.com>
2026-06-04 12:02:41 -0300

    Arquivos

---
+++ ex10_lista.py
@@ -0,0 +1,29 @@
+# Conversor de temperatura
+# Converter celsius para fahrenheit e vice-versa, deve perguntar para qual temperatura deve converterde
+
+
+def converção(origem,graus):
+    if origem == 'C':
+        calculo = (graus * 1.8) + 32
+    elif origem == 'F':
+        calculo = (graus - 32) *5/9
+
+    return calculo
+        
+    
+print("\n\n=== CONVERSOR DE TEMPERATURA ===")
+print('\n')
+
+
+opcao = str(input('A temperatura atual está em C(elsius) F(ahrenheit)?  ')).upper()
+graus = float(input('Quantos graus? '))
+if opcao == 'C':
+    celsius = converção(opcao, graus)
+    print(f'Sua temperatura em Fahrenheit é {celsius}')
+elif opcao == 'F':
+    fahrenheit = converção(opcao, graus)
+    print(f'Sua temperatura em Celsius é {fahrenheit}')
+else:
+    print('Algo deu errado!')
+
+    
---
+++ ex_01_lista.py
@@ -0,0 +1,3 @@
+nome = int(input('Qual é o seu nome? '))
+
+print(f('Olá {nome}!Bem-vindo(a) ao mundo Python!'))
